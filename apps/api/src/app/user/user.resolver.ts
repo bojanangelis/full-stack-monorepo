1e23ee23aed465fd6e4cf43395957190775a199b
bojanangelis <bojanangeleski98@gmail.com>
2023-11-09 15:22:35 +0100

    added backend graphql and users module

---
+++ apps/api/src/app/user/user.resolver.ts
@@ -0,0 +1,39 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import {
+  CreateOneUserArgs,
+  DeleteOneUserArgs,
+  FindUniqueUserArgs,
+  UpdateOneUserArgs,
+  User,
+} from '@full-stack/api/generated-db-types';
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => User)
+  createUser(@Args() createOneUserArgs: CreateOneUserArgs) {
+    return this.userService.create(createOneUserArgs);
+  }
+
+  @Query(() => [User])
+  users() {
+    return this.userService.findAll();
+  }
+
+  @Query(() => User)
+  findOne(@Args() findUniqueUserArgs: FindUniqueUserArgs) {
+    return this.userService.findOne(findUniqueUserArgs);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args() updateOneUserArgs: UpdateOneUserArgs) {
+    return this.userService.update(updateOneUserArgs);
+  }
+
+  @Mutation(() => User)
+  removeUser(@Args() deleteOneUserArgs: DeleteOneUserArgs) {
+    return this.userService.remove(deleteOneUserArgs);
+  }
+}
